28bb78da5cfd2086e3ae22a1d0953ad5888daa3c
gsh199449 <gsh199449@hotmail.com>
2013-08-17 21:32:27 +0800

    Some Bugs?
    Some Bugs?

--- src/com/gs/Lucene/Searcher.java
+++ src/com/gs/Lucene/Searcher.java
@@ -2,6 +2,15 @@ package com.gs.Lucene;
 
 public class Searcher {
 	public void search(){
-		
+		/*
+		 *创建Directory
+		 *创建IndexReader
+		 *根据IndexReader创建IndexSearcher
+		 *创建搜索的Query
+		 *根据Searcher搜索并返回TopDocs的文档
+		 *根据TopDocs获取SocoreDocs对象
+		 *根据Searcher和SocoreDocs获取具体的Document对象
+		 *根据Document对象获取需要的值
+		 */
 	}
 }
