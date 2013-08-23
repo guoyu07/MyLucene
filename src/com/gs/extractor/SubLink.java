56d2913b27795eed6dcc00ea96c3ab0794784b20
gsh199449 <gsh199449@hotmail.com>
2013-08-23 17:19:30 +0800

    sublink success

---
+++ src/com/gs/extractor/SubLink.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.gs.extractor;
+
+/**
+ * @author GaoShen
+ * @packageName com.gs.extractor
+ */
+public class SubLink {
+	public static String sub(String html){
+		char[] c = html.toCharArray();
+		if(c.length<12) return null;
+		String result = "";
+		if(html.substring(0, 13).equals("<a href=\"http"))
+			for(int i=9;i<c.length&&c[i] != '"';i++){
+				result += c[i];
+			}
+		return result;
+	}
+
+}
--- src/com/gs/test/TestGtmlParser.java
+++ src/com/gs/test/TestGtmlParser.java
@@ -8,6 +8,8 @@ import static org.junit.Assert.*;
 import org.htmlparser.parserapplications.LinkExtractor;
 import org.junit.Test;
 
+import com.gs.extractor.SubLink;
+
 /**
  * @author GaoShen
  * @packageName com.gs.test
@@ -21,5 +23,18 @@ public class TestGtmlParser {
 		s[0] = "http://news.qq.com";
 		LinkExtractor.main(s);
 	}
+	//<a href="http://mail.qq.com" target="_blank">
+	@Test
+	public void testExtractor(){
+		SubLink sub = new SubLink();
+		System.out.println(sub.sub("<a href=\"http://news.qq.com/a/20130821/007507.htm\" target=\"_blank\" class=\"hotPicStyle2\" bosszone=\"pic4\"><img src=\"http://img1.gtimg."));
+	}
 
+	@Test
+	public void testString(){
+		System.out.println("<a href=\"http://mail.qq.com\" target=\"_blank\">");
+		String s = "<a href=\"http://mail.qq.com\" target=\"_blank\">";
+		System.out.println(s.substring(0, 13));
+	}
+	
 }
